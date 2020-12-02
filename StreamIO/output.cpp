53092dda6fed47ad7b4933b84501440601c0ac4b
Vidit Shah <ml02100@yahoo.com>
2020-12-02 15:38:53 +0530

    output

--- StreamIO/output.cpp
+++ StreamIO/output.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+
 int main()
 {
-  std::cout << "Hello";
+  int limit = 100;
+  auto x = 7;
+
+  cout << "Hello "
+       << "Mr Stark!";
 }
